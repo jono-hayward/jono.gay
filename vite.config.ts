8c0e31304639c3200187ed52a453bc95fc58754c
Jono Hayward <gabs-crests0s@icloud.com>
2026-03-02 14:20:33 +1100

    Allow local testing

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,10 @@ import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [sveltekit(), devtoolsJson()] });
+export default defineConfig({
+	server: {
+		host: '0.0.0.0',
+		allowedHosts: true
+	},
+	plugins: [sveltekit(), devtoolsJson()]
+});
